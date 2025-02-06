d4bf758679ed4d0359d1fa70ec9ae79aadc6028b
Alexander Meshchenko <alexander.meshchenko@fastdev.se>
2025-02-06 13:19:26 +0100

    Initial Commit

---
+++ SmartPrintFix/SmartPrintFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPrintFixApp.swift
+//  SmartPrintFix
+//
+//  Created by Aleksandr Meshchenko on 06.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SmartPrintFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartPrintFixTests/SmartPrintFixTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPrintFixTests.swift
+//  SmartPrintFixTests
+//
+//  Created by Aleksandr Meshchenko on 06.02.25.
+//
+
+import Testing
+@testable import SmartPrintFix
+
+struct SmartPrintFixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
