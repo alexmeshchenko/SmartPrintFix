997cb198789226ba363dbc0ad464527e6ecd87f4
Alexander Meshchenko <alexander.meshchenko@fastdev.se>
2025-02-07 10:29:46 +0100

    [ADD] Initial commit.

---
+++ SmartPrintFix/PDFKitView.swift
@@ -0,0 +1,12 @@
+struct PDFKitView: NSViewRepresentable {
+    let document: PDFDocument
+    
+    func makeNSView(context: Context) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = document
+        pdfView.autoScales = true
+        return pdfView
+    }
+    
+    func updateNSView(_ nsView: PDFView, context: Context) {}
+}
